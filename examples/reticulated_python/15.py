3b23ede891878b2df1666876d71caebde2c369db
Ben Chung <ckfinite@gmail.com>
2015-08-30 16:01:49 -0400

    Fixed 15.... hopefully

--- examples/reticulated_python/15.py
+++ examples/reticulated_python/15.py
@@ -1,20 +1,23 @@
 @fields({'a':Dyn})
 class A:
 	a = 1
+	def setA(self:A, v:dyn):
+		self.a = v
 
 @fields({'a':Int})
 class B(A):
 	a = 1
+	setA(self:B, v:dyn):
+		# empty
 
-@fields({'a' : A, 'b' : B, 'i' : Int})
+@fields({'a' : A, 'b' : B})
 class C:
 	a = A()
 	b = B()
-	i = 2
 	def main(self:C) -> Int:
-		self.a = B()
-		self.b = self.a
-		self.a.a = "foo"
+		self.a = A()
+		self.b = a # a is now required to be an int
+		self.a.setA("hello") # should get a type violation
 		return self.b.a
 
 
