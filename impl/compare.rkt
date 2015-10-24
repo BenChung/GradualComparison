818150147eaafc3512462cb8411a12b448642adf
Ben Chung <ckfinite@gmail.com>
2015-10-24 17:28:37 -0400

    Work on the typed racket comparator

--- impl/compare.rkt
+++ impl/compare.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require redex)
 (require racket/match)
-
+(require racket/sandbox)
 ; core language
 ; class C extends D { l1 : T, ..., m(x:t, ...):T { e} ...
 ; e ::= l | x.l(e,...) | const | if e e e | e + e | new C(e, ...) | e.l = e
@@ -29,8 +29,18 @@
 
 (redex-check L p
              (term p)
-             #:print? #t #:attempts 10000)
+             #:print? #t #:attempts 10)
 
-(match (term (lookup 2 "foo")) [(quasiquote (lookup (unquote x) "foo")) x])
+(define evaluator (make-module-evaluator "#lang typed/racket"))
+(evaluator '(begin (: x Integer) (define x "foo") x))
+(match (term (lookup 2 "foo"))
+  [`(lookup ,x ,name) `(,x ,name)]
+  [`(call ,e1 ,name ,e2 ...) `(,name)]
+  [`(if ,e1 ,e2 ,e3) `(,e1 ,e2 ,e3)]
+  [`(oplus ,e1 ,e2) `(,e1 ,e2)]
+  [`(new ,name (,arg ...)) `(,name ,arg ...)]
+  [`(assign ,e1 ,name ,e2) `(,e1 ,name ,e2)]
+  [`cons `(,cons)]
+  )
 
 (display-to-file (quasiquote ((foo 1) 3 1 2)) "test.rkt" #:exists `replace)
