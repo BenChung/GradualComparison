3d79552672f0cb0d414d991ce2541b36e5a81d79
Ben Chung <ckfinite@gmail.com>
2016-09-26 18:44:47 -0400

    work

---
+++ examples/typed_racket/typed5.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket
+
+(define-type A (Class [foo (-> Integer)] [bar (-> Integer)]))
+(define-type B (Class [foo (-> Integer)] [baz (-> Integer)]))
+(: A% A)
+(define A% (class object%
+                (super-new)
+                (define/public (foo)
+                  2)
+                (define/public (bar)
+                  2)
+  ))
+
+(require/typed "simp1.rkt"
+               [id (-> (Instance A) (Instance B))]
+               [(id id2) (-> (Instance B) (Instance A))]
+               [(id id3) (-> Integer String)])
+
+(send (id2 (id (make-object A%))) foo)
+(send (id2 (id (make-object A%))) bar)
