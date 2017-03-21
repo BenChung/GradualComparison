30a355c8d0d74fa07691efee0d40ad4f3087e629
Benjamin Chung <ckfinite@gmail.com>
2017-03-21 16:32:04 -0400

    AST defn

---
+++ netImpl/kafkaimpl/AST.fs
@@ -0,0 +1,28 @@
+﻿module AST
+
+type Type =
+| Any
+| Class of string
+
+type Expr =
+| Var of string
+| This
+| That
+| NewExn of string * (Expr list)
+| GetF of Expr * string 
+| SetF of Expr * string * Expr
+| Call of Expr * Type * Type * string * Expr
+| DynCall of Expr * string * Expr
+| Cast of Type * Expr
+| Seq of Expr list
+
+type md = 
+| MDef of string * string * Type * Type * Expr
+| SDef of string * string * Type * Expr
+| GDef of string * Type * Expr
+
+type fd =
+| FDef of string * Type
+
+type k =
+| ClassDef of string * md list * fd list
