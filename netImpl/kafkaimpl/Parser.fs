0dd4d1bf165da45bb9e295eeabe3cbd605e9f405
Benjamin Chung <ckfinite@gmail.com>
2017-03-21 22:54:45 -0400

    Parser work

---
+++ netImpl/kafkaimpl/Parser.fs
@@ -0,0 +1,34 @@
+﻿module Parser
+open FParsec
+open AST
+
+let parse (prog:string) = 
+    let ws = spaces <|> (skipMany skipNewline)
+    let str_ws s = pstring s .>> ws
+    let id = many1Chars asciiLetter
+    let tpe = (pstring "any" |>> fun x -> Any) <|> (id |>> fun x -> Class x)
+    let term, termImpl = createParserForwardedToRef()
+    let expr = (pstring "this" |>> fun x -> This) <|> 
+               (pstring "that" |>> fun x -> That) <|> 
+               (pipe2 (pstring "new" >>. ws >>. id .>> ws .>> pstring "(")
+                     ((sepBy (ws >>. term) (ws >>. pstring ",")) .>> pstring ")")
+                     (fun className args -> NewExn(className, args))) <|>
+               (id |>> fun x -> Var x)
+    let callExpr = (attempt (id .>> ws .>> pstring "(" .>> ws .>> pstring ")") |>> (fun name -> fun exp -> GetF(exp,name))) <|>
+                   pipe2 (id .>> ws .>> pstring "(" .>> ws) (term .>> ws .>> pstring ")") (fun name arg -> fun exp -> SetF(exp, name, arg))
+    do termImpl := (attempt (pipe2 (expr .>> pstring ".") (sepBy callExpr (pstring ".")) (fun e l -> e))) <|> expr
+    let term = termImpl.Value
+    let fd = id .>> pstring ":" .>>. tpe
+    let md = pipe5 (id .>> pstring "(") 
+                   (ws >>. id) 
+                   (ws .>> pstring ":" >>. ws >>. tpe .>> pstring ")" .>> ws .>> pstring ":" .>> ws) 
+                   (tpe .>> ws .>> pstring "{" .>> ws) 
+                   ( term .>> ws .>> pstring "}")
+                   (fun name var tp t body -> MDef(name, var, tp, tp, body)) 
+    let gd = (id .>> pstring "(" .>> ws .>> pstring ")" 
+        .>> ws .>> pstring ":" .>> ws .>>. tpe .>> ws
+        .>> pstring "{" .>> ws .>>. term .>> ws .>> pstring "}")
+    
+    let clazz = ws >>. str_ws "class" >>. ws >>. id .>> ws .>> pstring "{" .>>. (sepEndBy (attempt (ws >>. fd)) skipNewline) .>>. (sepEndBy (ws >>. md) skipNewline) .>> pstring "}"
+    let program = (sepEndBy clazz skipNewline) .>> ws .>>. term .>> ws
+    run program prog
--- netImpl/kafkaimpl/Program.fs
+++ netImpl/kafkaimpl/Program.fs
@@ -1,8 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 open System
+open Parser
 
 [<EntryPoint>]
 let main argv = 
+    let res = Parser.parse @"
+class B {
+    f:D
+    m(x:any):any { new B(x, y, z) }
+    m(x:any):any { too.bar() }
+    m(x:any):any { too.bar(baz) }
+}
+class D {}
+hello"
     printfn "%A" argv
     0 // return an integer exit code
