c0c84a15ebcbde89f14f4002fad78f4da42ab86e
Benjamin Chung <ckfinite@gmail.com>
2017-03-22 14:32:17 -0400

    Change toolsversion and add example project

---
+++ netImpl/dyntest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dyntest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic a = new List<int>() { 1, 2, 3, 4 };
+            dynamic c = 2;
+            dynamic b = a.Count;
+            Console.WriteLine(b);
+        }
+    }
+}
