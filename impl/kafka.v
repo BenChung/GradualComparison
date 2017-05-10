797996dbd37fcb0e95614a048d8391de853be03a
Benjamin Chung <ckfinite@gmail.com>
2017-05-10 11:19:13 -0400

    More coq work

--- impl/kafka.v
+++ impl/kafka.v
@@ -269,22 +269,269 @@ Inductive WellFormedMethod : env -> heap -> ct -> md -> Prop :=
          HasType ((x,(class C1)) :: g) s k e (class C2) ->
          WellFormedMethod g s k (Method m x (class C1) (class C2) e).
 
+Inductive NoDupsMds : list md -> Prop :=
+| NDUPTC : forall m x C D e mds,
+    NoDupsMds mds -> 
+    (forall x' C' D' e', ~ In (Method m x' (class C') (class D') e') mds) ->
+    NoDupsMds ((Method m x (class C) (class D) e)::mds)
+| NDUPUT : forall m x e mds,
+    NoDupsMds mds ->
+    (forall x' e', ~In (Method m x' Any Any e') mds) ->
+    NoDupsMds ((Method m x Any Any e)::mds)
+| NDUPN : NoDupsMds nil.
+
+Inductive NoDupsFds : list fd -> Prop :=
+| NDUPFD : forall f t fds, NoDupsFds fds -> (forall t', ~ (In (Field f t') fds)) -> NoDupsFds ((Field f t)::fds).
+
 Inductive WellFormedClass : heap -> ct -> k -> Prop :=
-| WFWC : forall s k C mds fds, (forall fd, In fd fds -> WellFormedField k fd) ->
-                     (forall md, In md mds -> WellFormedMethod ((this, class C) :: nil) s k md) ->
-                       WellFormedClass s k (ClassDef C fds mds).
+| WFWC : forall s k C mds fds,
+    (forall fd, In fd fds -> WellFormedField k fd) ->
+    (forall md, In md mds -> WellFormedMethod ((this, class C) :: nil) s k md) ->
+    NoDupsFds fds -> NoDupsMds mds -> 
+    WellFormedClass s k (ClassDef C fds mds).
+
+Inductive WellFormedClassTable : heap -> ct -> ct -> Prop :=
+| WFCTC : forall c C fds mds s k kr, 
+    c = ClassDef C fds mds -> 
+    (forall fd' md', ~In (ClassDef C fd' md') kr) ->
+    (WellFormedClass s k c) -> WellFormedClassTable s k kr ->
+    WellFormedClassTable s k (c::kr)
+| WFNIL : forall s k, WellFormedClassTable s k nil.
 
 Inductive FieldRefWellFormed : ct -> heap -> list fd -> list ref -> Prop :=
-| FRWF_Cons : forall k s f t fds a a', HasType nil s k (Ref a) t -> FieldRefWellFormed k s ((Field f t)::fds) (a::a')
+| FRWF_Cons : forall k s f t fds a a', HasType nil s k (Ref a) t ->
+                                       FieldRefWellFormed k s fds a' ->
+                                       FieldRefWellFormed k s ((Field f t)::fds) (a::a')
 | FRWF_Nil : forall k s, FieldRefWellFormed k s nil nil.
 
-Inductive WellFormedHeap : ct -> heap -> Prop :=
-| WFH : forall k s, (forall a' C al, In (a', HCell(C,al)) s ->
-                                  (exists fds mds, (In (ClassDef C fds mds) k) /\ FieldRefWellFormed k s fds al)) ->
-               WellFormedHeap k s.
+Inductive WellFormedHeap : ct -> heap -> heap -> Prop :=
+| WFH_Cons : forall a s s' k aps C, WellFormedHeap k s s' -> (forall hc, ~In(a, hc) s') ->
+             FieldRefWellFormed k s (fields C k) aps -> WellFormedHeap k s ((a, HCell(C, aps))::s')
+| WFH_Nil : forall k s, WellFormedHeap k s nil.
                    
 Inductive WellFormedState : ct -> expr -> heap -> Prop :=
-| WFSWP : forall k e s t, HasType nil s k e t -> WellFormedHeap k s ->
-          (forall ki, In ki k -> WellFormedClass s k ki) ->
+| WFSWP : forall k e s t, HasType nil s k e t -> WellFormedHeap k s s ->
+          WellFormedClassTable s k k ->
           WellFormedState k e s.
- 
+
+Inductive Deref : list expr -> list ref -> Prop :=
+| DREF_Cons : forall a es ais, Deref es ais -> Deref ((Ref a)::es) (a::ais)
+| DREF_Nil : Deref nil nil.
+
+Inductive FieldIn : id -> type -> ref -> list fd -> list ref -> Prop :=
+| FieldIn_Next : forall f f' a a' fdr t t' ar,
+    f <> f' -> FieldIn f t a fdr ar -> FieldIn f t a ((Field f' t') :: fdr) (a' :: ar)
+| FieldIn_Here : forall f a t fdr ar, FieldIn f t a ((Field f t) :: fdr) (a :: ar).
+
+Lemma FieldsWFImpliesFieldIn : forall k s fds aps f t a,
+  FieldRefWellFormed k s fds aps -> FieldIn f t a fds aps -> HasType nil s k (Ref a) t.
+Proof.
+  intros k s fds aps f t a H1 H2. induction H1.
+  - destruct (Nat.eqb f f0) eqn:Hf.
+    + apply Nat.eqb_eq in Hf. subst. inversion H2; subst; try contradiction.
+      apply H.
+    + apply Nat.eqb_neq in Hf. inversion H2; subst; try contradiction.
+      apply IHFieldRefWellFormed in H11. apply H11.
+  - inversion H2.
+Qed.
+
+Lemma strong_weakening_of_type_wf : forall C C' fds mds k,
+    WellFormedType ((ClassDef C' fds mds) :: k) (class C) -> C <> C' ->
+    WellFormedType k (class C).
+Proof.
+  intros C C' fds mds k H1 H2. inversion H1; subst. inversion H3.
+  - inversion H. symmetry in H4. contradiction.
+  - eapply WFWTC. apply H.
+Qed.
+
+Lemma fields_gets_fields : forall C s k k' fds, WellFormedType k (class C) -> WellFormedClassTable s k' k ->
+    fields C k = fds <-> exists mds, In (ClassDef C fds mds) k.
+Proof.
+  intros C s k k' fds H H0. split; intros H1. 
+  - induction k as [n | k''].
+    + inversion H. subst. inversion H4.
+    + destruct k'' as [C' fds' mds']. simpl in H1. destruct (Nat.eqb C C') eqn:HCC.
+      * apply Nat.eqb_eq in HCC. subst. exists mds'. simpl. auto. 
+      * apply Nat.eqb_neq in HCC. simpl. inversion H. subst. apply strong_weakening_of_type_wf in H; eauto. 
+        apply IHk in H; eauto.
+        ** inversion H as [mds'' H5]. exists mds''. auto.
+        ** inversion H0. subst. eauto. 
+  - induction k as [|k''].
+    + inversion H1. inversion H2. 
+    + destruct k'' as [C' fds' mds']. unfold fields. destruct (C =? C') eqn:HCC.
+      * apply Nat.eqb_eq in HCC. subst. inversion H1. inversion H0.
+        ** subst. inversion H2.
+           *** inversion H3. subst. reflexivity. 
+           *** inversion H5. subst. unfold not in H6. apply H6 in H3. contradiction.
+      * apply IHk.
+        ** eapply strong_weakening_of_type_wf; eauto. apply Nat.eqb_neq in HCC. apply HCC.
+        ** inversion H0. eauto.
+        ** inversion H1. inversion H2.
+           *** inversion H3. apply Nat.eqb_neq in HCC. symmetry in H5. contradiction.
+           *** exists x. eauto. 
+Qed.
+
+Lemma heap_wf_field_access : forall k s s' a C aps f t a',
+    WellFormedHeap k s' s ->
+    WellFormedClassTable s' k k ->
+    In (a, HCell(C, aps)) s -> FieldIn f t a' (fields C k) aps ->
+  HasType nil s' k (Ref a') t.
+Proof.
+  intros k s s' a C aps f t a' H0 H1 H2 H3.
+  induction H0.
+  - destruct (Nat.eqb a a0) eqn:Haa.
+    + apply Nat.eqb_eq in Haa. subst. inversion H2.
+      * inversion H5. subst. remember (fields C k0) as fds.
+        eapply FieldsWFImpliesFieldIn with (fds:=fds) (aps:=aps) (f:=f); eauto.
+      * apply IHWellFormedHeap; eauto. 
+    + apply Nat.eqb_neq in Haa. inversion H2.
+      * inversion H5. subst. contradiction.
+      * apply IHWellFormedHeap; eauto.
+  - inversion H2.
+Qed.
+
+Inductive HeapWrite : id -> list ref -> heap -> heap -> Prop :=
+| HWC : forall a aps C aps' s, HeapWrite a aps ((a,HCell(C,aps'))::s) ((a,HCell(C,aps)) :: s)
+| HWNC : forall a a' aps aps' s s' C, a' <> a -> HeapWrite a aps s s' ->
+         HeapWrite a aps ((a', HCell(C,aps')) :: s) ((a', (HCell(C,aps'))) :: s').
+
+Inductive FieldWrite : id -> ref -> list ref -> list fd -> list ref -> Prop :=
+| FWC : forall a f a' aps fs t, FieldWrite f a (a'::aps) ((Field f t)::fs) (a::aps)
+| FWN : forall a f f' a' aps aps' fs t,
+    f <> f' -> FieldWrite f a aps fs aps' -> FieldWrite f a (a'::aps) ((Field f' t)::fs) (a' :: aps').
+
+Lemma FieldsNoDupsFds : forall s (k k' : ct) (C : id) fds mds, 
+    WellFormedClassTable s k' k ->
+    In (ClassDef C fds mds) k ->
+    fds = (fields C k) ->
+    NoDupsFds fds.
+Proof.
+  intros s k k' C fds mds H H0 H1. induction k.
+  - inversion H0.
+  - inversion H0.
+    + inversion H. subst. inversion H9. subst. inversion H2. subst. rewrite <- H4. unfold fields.
+      rewrite Nat.eqb_refl. apply H12.
+    + apply IHk; eauto.
+      * inversion H. subst. apply H10.
+      * clear H1. clear IHk. clear H0. symmetry. rewrite fields_gets_fields with (s := s) (k' := k').
+        ** exists mds. apply H2. 
+        ** eapply WFWTC.  apply H2.
+        ** inversion H. apply H8. 
+Qed.
+
+Lemma FieldWriteWellFormed : forall (k k' : ct) s a fds ais ais' t f,
+    FieldRefWellFormed k s fds ais ->
+    HasType nil s k (Ref a) t ->
+    In (Field f t) fds ->
+    NoDupsFds fds ->
+    FieldWrite f a ais fds ais' ->
+    FieldRefWellFormed k s fds ais'.
+Proof.
+  intros. induction H3.
+  - inversion H1.
+    + inversion H3. subst. apply FRWF_Cons; eauto.
+      * inversion H. apply H12. 
+    + inversion H2. subst. unfold not in H8. apply H8 in H3. contradiction.
+  - apply FRWF_Cons.
+    + inversion H. apply H9.
+    + apply IHFieldWrite; eauto.
+      * inversion H. apply H13.
+      * inversion H1. 
+        ** inversion H5. symmetry in H7. contradiction.
+        ** apply H5.
+      * inversion H2. apply H7.
+Qed.
+
+Lemma no_two_classes : forall s k C fds mds fds' mds',
+    WellFormedClassTable s k k -> In (ClassDef C fds mds) k -> In (ClassDef C fds' mds') k ->
+    fds = fds' /\ mds = mds'.
+Proof.
+  intros. induction H.
+  - subst. destruct (Nat.eqb C C0) eqn:HCC.
+    + apply Nat.eqb_eq in HCC. subst. inversion H0.
+      * inversion H. subst. inversion H1.
+        ** inversion H5. auto.
+        ** apply H2 in H5. contradiction. 
+      * apply H2 in H. contradiction.
+    + apply Nat.eqb_neq in HCC. inversion H0.
+      * inversion H. subst. contradiction.
+      * apply IHWellFormedClassTable in H.
+        ** apply H.
+        ** inversion H1.
+           *** inversion H5. subst. contradiction.
+           *** apply H5.
+  - inversion H0. 
+Qed.
+
+Lemma heap_write_still_in : forall a s s' a' aps' hc,
+  In (a, hc) s -> a <> a' -> HeapWrite a' aps' s s' -> In (a, hc) s'.
+Proof.
+  intros. induction H1.
+  - inversion H.
+    + inversion H1. subst. contradiction.
+    + apply in_cons. apply H1. 
+  - inversion H.
+    + inversion H3. subst. apply in_eq. 
+    + apply IHHeapWrite in H3.
+      * apply in_cons. apply H3.
+      * apply H0.
+Qed.
+
+Lemma heap_write_now_in : forall a k s s' s'' aps aps' C,
+    WellFormedHeap k s'' s -> In (a, HCell(C, aps)) s -> HeapWrite a aps' s s' -> In (a, HCell(C, aps')) s'.
+Proof.
+  intros. induction H1.
+  - destruct (Nat.eqb C C0) eqn:HCC.
+    + apply Nat.eqb_eq in HCC. subst. apply in_eq.
+    + apply Nat.eqb_neq in HCC. inversion H0.
+      * inversion H1. subst. contradiction.
+      * inversion H. subst. apply H9 in H1. contradiction.
+  - apply in_cons. apply IHHeapWrite.
+    + inversion H. subst. apply H8.
+    + inversion H0.
+      * inversion H3. subst. contradiction.
+      * apply H3.
+Qed.
+
+(* does not work after this point *)
+
+Lemma fieldwf_implies_heapwf : forall k s sp s' a' C aps aps' fds,
+  WellFormedHeap k (sp++s) s ->
+  In (a', HCell(C,aps)) s ->
+  fds = fields C k ->
+  FieldRefWellFormed k (sp ++ s) fds aps' ->
+  HeapWrite a' aps' s s' ->
+  WellFormedHeap k (sp ++ s') s'.
+Proof.
+  intros k s sp s' a C aps aps' fds.
+  intros Hwfh Hin Hfields Hfieldswf Hwrite.
+  generalize dependent sp. 
+  induction Hwrite.
+  - intros. remember
+    
+Lemma write_field : forall s s' sp k a a' aps aps' t C f,
+    WellFormedHeap k (sp ++ s) s ->
+    WellFormedClassTable (sp ++ s) k k ->
+    HasType nil (sp ++ s) k (Ref a) t ->
+    In (Field f t) (fields C k) ->
+    In (a', HCell(C, aps)) s ->
+    FieldWrite f a aps (fields C k) aps' ->
+    HeapWrite a' aps' s s' ->
+    WellFormedHeap k (sp ++ s') s'.
+Proof.
+  intros s s' sp k a a' aps aps' t C f.
+  intros Hwfh Hwfct Hht Hfin Hobj Hwrite Hhwrite. generalize dependent sp. induction Hhwrite.
+  - intros. apply WFH_Cons.
+    + inversion Hwfh. apply H4. 
+      
+Inductive Steps : ct -> expr -> heap -> ct -> expr -> heap -> Prop :=
+| SNew : forall a' s s' C k ais ais', (forall hc, ~ (In (a',hc) s)) ->
+                                      Deref ais ais' ->
+                                      s' = (a', HCell(C, ais'))::s ->
+                                      Steps k (New C ais) s k (Ref a') s'
+| SRead : forall a C aps s k f a', 
+    In (a, HCell(C, aps)) s ->
+    FieldIn f a' (fields C k) aps ->
+    Steps k (GetF (Ref a) f) s k (Ref a') s
+| SWrite : forall a C aps s k f a',
+. 
