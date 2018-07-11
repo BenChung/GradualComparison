67cefbf30629aa68d3a937fc4b5bac694037ed30
jan vitek <janvite@yahoo.com>
2018-07-11 23:48:27 +0200

    clean up

---
+++ writing/Purgatory/figures/make-figures.bat
@@ -0,0 +1,4 @@
+racket format-racket.rkt ..\..\examples\typed_racket\untyped1.rkt untyped-rkt1.pdf
+racket format-racket.rkt ..\..\examples\typed_racket\typed1.rkt typed-rkt1.pdf
+racket format-racket.rkt ..\..\examples\typed_racket\typed2.rkt typed-rkt2.pdf
+racket format-racket.rkt internal2.rkt internal.pdf
